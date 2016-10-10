97fb1ec14db5b13da83164f56ef1e3cccac7cb14
BinRui Guan <binrui.guan@hirede.com>
2016-10-10 16:03:46 +0800

    [build] 0.0.6

--- dest/knockout.register.cjs.js
+++ dest/knockout.register.cjs.js
@@ -1,69 +1,116 @@
 'use strict';
 
-var defaultSpy = document.createComment('spy');
+Object.defineProperty(exports, '__esModule', { value: true });
 
-function getVmForNode(node) {
-    var spy = node.lastElementChild || defaultSpy;
-    var useDefault = spy === defaultSpy;
+var literalRE = /^(?:true|false|null|NaN|Infinity|[\+\-]?\d?)$/i;
 
-    if (useDefault) {
-        node.appendChild(spy);
+// no-ops function
+function noop() {}
+
+// has own property and not falsy value
+function exist(target, key) {
+    return target[key] && target.hasOwnProperty(key);
+}
+
+// my-name => myName
+function normalize(name) {
+    return name.split(/\-/g).map(function (word, i) {
+        return i > 0 ? word.charAt(0).toUpperCase() + word.substr(1) : word;
+    }).join('');
+}
+
+// function type checker
+function isFunction(target) {
+    return typeof target === 'function';
+}
+
+// string type checker
+function isString(target) {
+    return typeof target === 'string';
+}
+
+// parse to string to primitive value
+//
+// "true" => true
+// "false" => false
+// "null" => null
+//
+// @param {String} value
+// @return {Any}
+function toPrimitive(value) {
+    if (typeof value !== 'string') {
+        return value;
+    } else if (value === 'True') {
+        return true;
+    } else if (value === 'False') {
+        return false;
     }
 
-    var vm = ko.dataFor(spy);
+    if (literalRE.test(value)) {
+        return new Function('return ' + value)();
+    } else {
+        return value;
+    }
+}
 
-    if (useDefault) {
-        node.removeChild(spy);
+// iterate dict with given iterator
+//
+// @param {Object} dict
+// @param {Function} iterator
+function eachDict(dict, iterator) {
+    if (!dict) {
+        return;
     }
 
-    return vm;
-}
-
-function querySelector(selector) {
-    var context = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : document;
-
-    return getVmForNode(context.querySelector(selector));
-}
-
-function querySelectorAll(selector) {
-    var context = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : document;
-
-    var nodes = ko.utils.makeArray(context.querySelectorAll(selector));
-
-    return ko.utils.arrayMap(nodes, function (node) {
-        return getVmForNode(node);
+    ko.utils.arrayForEach(Object.keys(dict), function (key) {
+        iterator(key, dict[key], dict);
     });
 }
 
-function getElementById() {
-    return getVmForNode(context.getElementById(selector));
-}
+function apply(selector, contextNode) {
+    if (isString(contextNode)) {
+        contextNode = document.querySelector(contextNode);
+    }
 
-function getElementsByTagName(selector) {
-    var context = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : document;
+    var nodes = (contextNode || document).querySelectorAll(selector);
 
-    var nodes = ko.utils.makeArray(context.getElementsByTagName(selector));
+    ko.utils.arrayForEach(nodes, function (node) {
+        var bindingStatements = null;
+        var bindingAttributes = node.getAttribute('data-bind') || '';
 
-    return ko.utils.arrayMap(nodes, function (node) {
-        return getVmForNode(node);
+        if (bindingAttributes.indexOf(/\bskip\b\s*:/g) > -1) {
+            return;
+        }
+
+        ko.applyBindings(null, node);
+        bindingStatements = bindingAttributes ? bindingAttributes.split(',') : [];
+        bindingStatements.push('skip: true');
+        node.setAttribute('data-bind', bindingStatements.join(','));
     });
 }
 
-function getElementsByClassName(selector) {
-    var context = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : document;
+// extend ko.components
+ko.components.apply = ko.components.apply || apply;
 
-    var nodes = ko.utils.makeArray(context.getElementsByClassName(selector));
-
-    return ko.utils.arrayMap(nodes, function (node) {
-        return getVmForNode(node);
+// create computed observalbes on context
+//
+// @param {Object} context
+// @param {Object} methods
+function computedAll(context, methods) {
+    eachDict(methods, function (name, method) {
+        context[name] = ko.computed(method, context);
     });
 }
 
-ko.components.querySelector = querySelector;
-ko.components.querySelectorAll = querySelectorAll;
-ko.components.getElementById = getElementById;
-ko.components.getElementsByTagName = getElementsByTagName;
-ko.components.getElementsByClassName = getElementsByClassName;
+// create pure computed observalbes on context
+//
+// @param {Object} context
+// @param {Object} methods
+function pureComputedAll(context, methods) {
+    eachDict(methods, function (name, method) {
+        context[name] = ko.pureComputed(method, context);
+    });
+}
 
 function interopDefault(ex) {
 	return ex && typeof ex === 'object' && 'default' in ex ? ex['default'] : ex;
@@ -100,83 +147,19 @@ module.exports = function (css, options) {
 
 var insert = interopDefault(index);
 
-var literalRE = /^(?:true|false|null|NaN|Infinity|[\+\-]?\d?)$/i;
-var utils = ko.utils;
-
-// no-ops function
-function noop() {}
-
 // empty component template
 var emptyTemplate = '<!-- empty template -->';
 
 // throw error with plugin name
+//
+// @param {String} message
 function throwError(message) {
     throw new Error('knockout.register: ' + message);
-}
-
-// has own property and not falsy value
-function exist(target, key) {
-    return target[key] && target.hasOwnProperty(key);
-}
-
-// function type checker
-function isFunction(target) {
-    return typeof target === 'function';
-}
-
-// string type checker
-function isString(target) {
-    return typeof target === 'string';
 }
 
 // insert dom stylesheet
 function insertCss() {
     return insert.apply(null, arguments);
-}
-
-// my-name => myName
-function normalize(name) {
-    return name.split(/\-/g).map(function (word, i) {
-        return i > 0 ? word.charAt(0).toUpperCase() + word.substr(1) : word;
-    }).join('');
-}
-
-// parse to string to primitive value
-//
-// "true" => true
-// "false" => false
-// "null" => null
-//
-// @param {String} value
-// @return {Any}
-function toPrimitive(value) {
-    if (typeof value !== 'string') {
-        return value;
-    } else if (value === 'True') {
-        return true;
-    } else if (value === 'False') {
-        return false;
-    }
-
-    if (literalRE.test(value)) {
-        return new Function('return ' + value)();
-    } else {
-        return value;
-    }
-}
-
-// iterate dict with given iterator
-//
-// @param {Object} dict
-// @param {Function} iterator
-function eachDict(dict, iterator) {
-    if (!dict) {
-        return;
-    }
-
-    utils.arrayForEach(Object.keys(dict), function (key) {
-        iterator(key, dict[key], dict);
-    });
 }
 
 // robust mixin
@@ -203,16 +186,16 @@ function mixin(dest, opts, mixins) {
     // cache last opts
     dest.opts = opts = opts || {};
 
-    utils.arrayForEach(mixins, function (mixin) {
+    ko.utils.arrayForEach(mixins, function (mixin) {
         var mixinCopy = null;
 
         // pre mix
         if (isFunction(mixin.preMix)) {
-            mixinCopy = utils.extend({}, mixin);
+            mixinCopy = ko.utils.extend({}, mixin);
             mixinCopy.preMix(opts);
         }
 
-        utils.extend(dest, mixinCopy || mixin);
+        ko.utils.extend(dest, mixinCopy || mixin);
 
         // post mix
         if (isFunction(mixin.postMix)) {
@@ -226,47 +209,315 @@ function mixin(dest, opts, mixins) {
     return dest;
 }
 
-function apply(selector, contextNode) {
-    if (isString(contextNode)) {
-        contextNode = document.querySelector(contextNode);
+// extend ko.utils
+ko.utils.mixin = ko.utils.mixin || mixin;
+ko.utils.insertCss = ko.utils.insertCss || insertCss;
+
+var defaultSpy = document.createComment('spy');
+
+function getVmForNode(node) {
+    var spy = node.lastElementChild || defaultSpy;
+    var useDefault = spy === defaultSpy;
+
+    if (useDefault) {
+        node.appendChild(spy);
     }
 
-    var nodes = (contextNode || document).querySelectorAll(selector);
+    var vm = ko.dataFor(spy);
 
-    ko.utils.arrayForEach(nodes, function (node) {
-        var bindingStatements = null;
-        var bindingAttributes = node.getAttribute('data-bind') || '';
+    if (useDefault) {
+        node.removeChild(spy);
+    }
 
-        if (bindingAttributes.indexOf(/\bskip\b\s*:/g) > -1) {
-            return;
+    return vm;
+}
+
+// query element by selector
+//
+// @param {String} selector
+// @param {Node} context
+function querySelector(selector) {
+    var context = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : document;
+
+    return getVmForNode(context.querySelector(selector));
+}
+
+// query elements by selector
+//
+// @param {String} selector
+// @param {Node} context
+function querySelectorAll(selector) {
+    var context = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : document;
+
+    var nodes = ko.utils.makeArray(context.querySelectorAll(selector));
+
+    return ko.utils.arrayMap(nodes, function (node) {
+        return getVmForNode(node);
+    });
+}
+
+// query element by id
+//
+// @param {String} selector
+// @param {Node} context
+function getElementById() {
+    return getVmForNode(context.getElementById(selector));
+}
+
+// query elements by tag name
+//
+// @param {String} selector
+// @param {Node} context
+function getElementsByTagName(selector) {
+    var context = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : document;
+
+    var nodes = ko.utils.makeArray(context.getElementsByTagName(selector));
+
+    return ko.utils.arrayMap(nodes, function (node) {
+        return getVmForNode(node);
+    });
+}
+
+// query elements by class name
+//
+// @param {String} selector
+// @param {Node} context
+function getElementsByClassName(selector) {
+    var context = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : document;
+
+    var nodes = ko.utils.makeArray(context.getElementsByClassName(selector));
+
+    return ko.utils.arrayMap(nodes, function (node) {
+        return getVmForNode(node);
+    });
+}
+
+// ref vm
+//
+// @param {String} query
+// @param {Node} context
+function ref(query, context) {
+    return ko.components.querySelector(query, context || this.componentInfo.element);
+}
+
+// ref vms
+//
+// @param {String} query
+// @param {Node} context
+function refs(query, context) {
+    return ko.components.querySelectorAll(query, context || this.componentInfo.element);
+}
+
+// extend ko.components
+ko.components.querySelector = querySelector;
+ko.components.querySelectorAll = querySelectorAll;
+ko.components.getElementById = getElementById;
+ko.components.getElementsByTagName = getElementsByTagName;
+ko.components.getElementsByClassName = getElementsByClassName;
+
+var asyncGenerator = function () {
+  function AwaitValue(value) {
+    this.value = value;
+  }
+
+  function AsyncGenerator(gen) {
+    var front, back;
+
+    function send(key, arg) {
+      return new Promise(function (resolve, reject) {
+        var request = {
+          key: key,
+          arg: arg,
+          resolve: resolve,
+          reject: reject,
+          next: null
+        };
+
+        if (back) {
+          back = back.next = request;
+        } else {
+          front = back = request;
+          resume(key, arg);
         }
+      });
+    }
 
-        ko.applyBindings(null, node);
-        bindingStatements = bindingAttributes ? bindingAttributes.split(',') : [];
-        bindingStatements.push('skip: true');
-        node.setAttribute('data-bind', bindingStatements.join(','));
-    });
-}
+    function resume(key, arg) {
+      try {
+        var result = gen[key](arg);
+        var value = result.value;
 
-// create computed observalbes on context
-//
-// @param {Object} context
-// @param {Object} methods
-function computedAll(context, methods) {
-    eachDict(methods, function (name, method) {
-        context[name] = ko.computed(method, context);
-    });
-}
+        if (value instanceof AwaitValue) {
+          Promise.resolve(value.value).then(function (arg) {
+            resume("next", arg);
+          }, function (arg) {
+            resume("throw", arg);
+          });
+        } else {
+          settle(result.done ? "return" : "normal", result.value);
+        }
+      } catch (err) {
+        settle("throw", err);
+      }
+    }
 
-// create pure computed observalbes on context
-//
-// @param {Object} context
-// @param {Object} methods
-function pureComputedAll(context, methods) {
-    eachDict(methods, function (name, method) {
-        context[name] = ko.pureComputed(method, context);
-    });
-}
+    function settle(type, value) {
+      switch (type) {
+        case "return":
+          front.resolve({
+            value: value,
+            done: true
+          });
+          break;
+
+        case "throw":
+          front.reject(value);
+          break;
+
+        default:
+          front.resolve({
+            value: value,
+            done: false
+          });
+          break;
+      }
+
+      front = front.next;
+
+      if (front) {
+        resume(front.key, front.arg);
+      } else {
+        back = null;
+      }
+    }
+
+    this._invoke = send;
+
+    if (typeof gen.return !== "function") {
+      this.return = undefined;
+    }
+  }
+
+  if (typeof Symbol === "function" && Symbol.asyncIterator) {
+    AsyncGenerator.prototype[Symbol.asyncIterator] = function () {
+      return this;
+    };
+  }
+
+  AsyncGenerator.prototype.next = function (arg) {
+    return this._invoke("next", arg);
+  };
+
+  AsyncGenerator.prototype.throw = function (arg) {
+    return this._invoke("throw", arg);
+  };
+
+  AsyncGenerator.prototype.return = function (arg) {
+    return this._invoke("return", arg);
+  };
+
+  return {
+    wrap: function (fn) {
+      return function () {
+        return new AsyncGenerator(fn.apply(this, arguments));
+      };
+    },
+    await: function (value) {
+      return new AwaitValue(value);
+    }
+  };
+}();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var _extends = Object.assign || function (target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = arguments[i];
+
+    for (var key in source) {
+      if (Object.prototype.hasOwnProperty.call(source, key)) {
+        target[key] = source[key];
+      }
+    }
+  }
+
+  return target;
+};
+
+var get = function get(object, property, receiver) {
+  if (object === null) object = Function.prototype;
+  var desc = Object.getOwnPropertyDescriptor(object, property);
+
+  if (desc === undefined) {
+    var parent = Object.getPrototypeOf(object);
+
+    if (parent === null) {
+      return undefined;
+    } else {
+      return get(parent, property, receiver);
+    }
+  } else if ("value" in desc) {
+    return desc.value;
+  } else {
+    var getter = desc.get;
+
+    if (getter === undefined) {
+      return undefined;
+    }
+
+    return getter.call(receiver);
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var set = function set(object, property, value, receiver) {
+  var desc = Object.getOwnPropertyDescriptor(object, property);
+
+  if (desc === undefined) {
+    var parent = Object.getPrototypeOf(object);
+
+    if (parent !== null) {
+      set(parent, property, value, receiver);
+    }
+  } else if ("value" in desc && desc.writable) {
+    desc.value = value;
+  } else {
+    var setter = desc.set;
+
+    if (setter !== undefined) {
+      setter.call(receiver, value);
+    }
+  }
+
+  return value;
+};
 
 var invalidAttrNameRE = /^(?:data-[\w-]+|params|id|class|style)\b/i;
 var observableAttrNameRE = /^k-([\w\-]+)/i;
@@ -330,7 +581,7 @@ function pluck(node) {
         var nodeValue = node.nodeValue;
 
         if (invalidAttrNameRE.test(nodeName)) {
-            return;
+            return params;
         }
 
         if (observableAttrNameRE.test(nodeName)) {
@@ -351,7 +602,7 @@ function pluck(node) {
         bindingContext = ko.contextFor(node);
     }
 
-    return Object.assign(result, eventBindingString && pluckEventParams(bindingContext, eventBindingString), observableBindingString && pluckObservableParams(bindingContext, observableBindingString));
+    return _extends(result, eventBindingString && pluckEventParams(bindingContext, eventBindingString), observableBindingString && pluckObservableParams(bindingContext, observableBindingString));
 }
 
 var modulePolyfill = {
@@ -365,10 +616,11 @@ var modulePolyfill = {
 // @param {Object} module Transiton component module
 // @return {Object} Native component module
 function transform(module) {
-    var _Object$assign = Object.assign({}, modulePolyfill, module);
+    var _Object$assign = _extends({}, modulePolyfill, module);
 
+    var name = _Object$assign.name;
     var constructor = _Object$assign.constructor;
-    var defaults = _Object$assign.defaults;
+    var defaults$$1 = _Object$assign.defaults;
     var mixins = _Object$assign.mixins;
     var methods = _Object$assign.methods;
     var computed = _Object$assign.computed;
@@ -378,12 +630,18 @@ function transform(module) {
 
 
     insertCss(module.style);
-    methods && Object.assign(constructor.prototype, methods);
+    _extends(constructor.prototype, {
+        ref: ref,
+        refs: refs,
+        ready: noop
+    }, methods);
 
     return {
         viewModel: {
             createViewModel: function createViewModel(params, componentInfo) {
-                var opts = Object.assign(ko.toJS(params), defaults, pluck(componentInfo.element));
+                componentInfo.name = name;
+
+                var opts = _extends({}, defaults$$1, ko.toJS(params), pluck(componentInfo.element));
                 var vm = new constructor(opts, componentInfo);
 
                 mixins && mixin(vm, opts, mixins);
@@ -391,7 +649,7 @@ function transform(module) {
                 pureComputed && pureComputedAll(vm, pureComputed);
 
                 vm.$opts = opts;
-                vm.$defaults = defaults;
+                vm.$defaults = defaults$$1;
                 vm.$info = vm.componentInfo = componentInfo;
 
                 delete vm.$opts['$raw'];
@@ -415,7 +673,7 @@ function register(module) {
     }
 
     // native component module
-    if (typeof module === 'string') {
+    if (isString(module)) {
         return ko.components._register(arguments[0], arguments[1]);
     }
 
@@ -432,10 +690,3 @@ function register(module) {
 
 ko.components._register = ko.components._register || ko.components.register;
 ko.components.register = register;
-
-// extend knockout utils
-ko.utils.mixin = ko.utils.mixin || mixin;
-ko.utils.insertCss = ko.utils.insertCss || insertCss;
-
-// extend knockout components
-ko.components.apply = ko.components.apply || apply;
