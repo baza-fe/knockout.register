f7c775370c731d1c36852bfdb933eb7493dccd7e
BinRui Guan <binrui.guan@gmail.com>
2016-10-16 21:19:55 +0800

    Remove IE and Safari from test suit

--- build/karma.config.js
+++ build/karma.config.js
@@ -21,10 +21,10 @@ module.exports = function(config) {
     plugins: [
         'karma-jasmine',
         'karma-chrome-launcher',
-        'karma-ie-launcher',
         'karma-phantomjs-launcher',
         'karma-firefox-launcher',
-        'karma-safari-launcher',
+        // 'karma-ie-launcher',
+        // 'karma-safari-launcher',
         'karma-rollup-preprocessor'
     ],
 
@@ -83,7 +83,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome', 'Firefox', 'IE', 'Safari', 'PhantomJS'],
+    browsers: ['Chrome', 'Firefox', 'PhantomJS'],
 
 
     // Continuous Integration mode
