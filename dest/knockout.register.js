4f82a05e87a8fd7a2b631fea045b1f7396e779df
BinRui Guan <binrui.guan@gmail.com>
2016-11-06 22:36:20 +0800

    [build] 0.2.2

--- dest/knockout.register.js
+++ dest/knockout.register.js
@@ -1075,6 +1075,7 @@ var slotLoader = {
 ko.components.loaders.unshift(slotLoader);
 
 var manualRenderFlagName = '__manual_render_flag__';
+var customDisposeMethodName = '__custom_dispose_method__';
 var beginManualRenderTag = document.createComment('ko if: $data.' + manualRenderFlagName);
 var beginAfterRenderTag = document.createComment('ko template: { afterRender: ready.bind($data) }');
 var endTag = document.createComment('/ko');
@@ -1088,6 +1089,16 @@ var base = {
     render: function render() {
         this[manualRenderFlagName](false);
         this[manualRenderFlagName](true);
+    },
+    dispose: function dispose() {
+        if (isFunction(this[customDisposeMethodName])) {
+            this[customDisposeMethodName]();
+        }
+
+        if (this.componentInfo) {
+            this.componentInfo.element = null;
+            this.componentInfo = null;
+        }
     }
 };
 
@@ -1104,6 +1115,8 @@ var lifeComponentLoader = {
                 vm[manualRenderFlagName] = ko.observable(true).extend({ notify: 'always' });
                 vm.ready = vm.ready || noop;
                 vm.created = vm.created || noop;
+                vm[customDisposeMethodName] = vm.dispose || noop;
+                vm.dispose = base.dispose;
                 vm.ref = base.ref;
                 vm.refs = base.refs;
                 vm.render = base.render;
