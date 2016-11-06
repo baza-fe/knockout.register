3ac697cb9f13675301a561d03ffe46cfad752f16
BinRui Guan <binrui.guan@gmail.com>
2016-11-06 22:30:05 +0800

    Update test for dispose

--- test/specs/component_avatar_lifecycle_spec.js
+++ test/specs/component_avatar_lifecycle_spec.js
@@ -3,49 +3,9 @@ import Avatar from '../samples/components/avatar';
 
 describe('avatar', () => {
     const body = document.querySelector('body');
+    const context = { trigger: ko.observable(true) };
 
     beforeEach(() => {
-        body.innerHTML = `
-            <div id="test">
-                <avatar></avatar>
-            </div>
-        `;
-    });
-
-    afterEach(() => {
-        body.innerHTML = '';
-        ko.components.unregister(Avatar.name);
-    });
-
-    it('should create lifecycle methods', () => {
-        ko.components.register(Avatar);
-        ko.applyBindings(null, document.querySelector('#test'));
-        const vm = ko.components.querySelector('avatar');
-
-        expect(typeof vm.ref === 'function').toBe(true);
-        expect(typeof vm.refs === 'function').toBe(true);
-        expect(typeof vm.ready === 'function').toBe(true);
-        expect(typeof vm.render === 'function').toBe(true);
-        expect(typeof vm.created === 'function').toBe(true);
-    });
-
-    it('should invoke ready', () => {
-        Avatar.methods.ready = jasmine.createSpy('ready');
-        ko.components.register(Avatar);
-        ko.applyBindings(null, document.querySelector('#test'));
-        expect(Avatar.methods.ready).toHaveBeenCalled();
-    });
-
-    it('should invoke created', () => {
-        Avatar.methods.created = jasmine.createSpy('created');
-        ko.components.register(Avatar);
-        ko.applyBindings(null, document.querySelector('#test'));
-        expect(Avatar.methods.ready).toHaveBeenCalled();
-    });
-
-    it('should invoke dispose', () => {
-        const context = { trigger: ko.observable(true) };
-
         body.innerHTML = `
             <div id="test">
                 <!-- ko if: trigger -->
@@ -53,6 +13,42 @@ describe('avatar', () => {
                 <!-- /ko -->
             </div>
         `;
+    });
+
+    afterEach(() => {
+        body.innerHTML = '';
+        ko.components.unregister(Avatar.name);
+        context.trigger(true);
+    });
+
+    it('should create lifecycle methods', () => {
+        ko.components.register(Avatar);
+        ko.applyBindings(context, document.querySelector('#test'));
+        const vm = ko.components.querySelector('avatar');
+
+        expect(typeof vm.ref === 'function').toBe(true);
+        expect(typeof vm.refs === 'function').toBe(true);
+        expect(typeof vm.ready === 'function').toBe(true);
+        expect(typeof vm.render === 'function').toBe(true);
+        expect(typeof vm.created === 'function').toBe(true);
+        expect(typeof vm.dispose === 'function').toBe(true);
+    });
+
+    it('should invoke ready', () => {
+        Avatar.methods.ready = jasmine.createSpy('ready');
+        ko.components.register(Avatar);
+        ko.applyBindings(context, document.querySelector('#test'));
+        expect(Avatar.methods.ready).toHaveBeenCalled();
+    });
+
+    it('should invoke created', () => {
+        Avatar.methods.created = jasmine.createSpy('created');
+        ko.components.register(Avatar);
+        ko.applyBindings(context, document.querySelector('#test'));
+        expect(Avatar.methods.ready).toHaveBeenCalled();
+    });
+
+    it('should invoke dispose', () => {
         Avatar.methods.dispose = jasmine.createSpy('dispose');
         ko.components.register(Avatar);
         ko.applyBindings(context, document.querySelector('#test'));
@@ -61,19 +57,9 @@ describe('avatar', () => {
         expect(Avatar.methods.dispose).toHaveBeenCalled();
     });
 
-    it('should lifecycle in correct sequence', () => {
-        const order = [];
-
-        Avatar.methods.ready = () => { order.push('ready') };
-        Avatar.methods.created = () => { order.push('created') };
-        ko.components.register(Avatar);
-        ko.applyBindings(null, document.querySelector('#test'));
-        expect(order).toEqual([ 'created', 'ready' ]);
-    });
-
     it('should render works', () => {
         ko.components.register(Avatar);
-        ko.applyBindings(null, document.querySelector('#test'));
+        ko.applyBindings(context, document.querySelector('#test'));
         const vm = ko.components.querySelector('avatar');
 
         vm.first = 'foo';
@@ -83,5 +69,30 @@ describe('avatar', () => {
         vm.render();
         expect(document.querySelector('.avatar-first').textContent).toBe(vm.first);
         expect(document.querySelector('.avatar-last').textContent).toBe(vm.last);
+    });
+
+    it('should dispose works', () => {
+        ko.components.register(Avatar);
+        ko.applyBindings(context, document.querySelector('#test'));
+        const vm = ko.components.querySelector('avatar');
+        const componentInfo = vm.componentInfo;
+
+        expect(vm.componentInfo).toBeDefined();
+        expect(vm.componentInfo.element).toBeDefined();
+        context.trigger(false);
+        expect(vm.componentInfo).toBe(null);
+        expect(componentInfo.element).toBe(null);
+    });
+
+    it('should lifecycle in correct sequence', () => {
+        const order = [];
+
+        Avatar.methods.ready = () => { order.push('ready') };
+        Avatar.methods.created = () => { order.push('created') };
+        Avatar.methods.dispose = () => { order.push('dispose') };
+        ko.components.register(Avatar);
+        ko.applyBindings(context, document.querySelector('#test'));
+        context.trigger(false);
+        expect(order).toEqual([ 'created', 'ready', 'dispose' ]);
     });
 });
